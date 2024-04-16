54774d54dc709b7ab5e78552cdb0e34364da80ab
michaelarnouse <michael.arnouse@richmond.edu>
2024-04-16 12:31:17 -0400

    fixing pagination

--- src/components/Marketplace/Marketplace.jsx
+++ src/components/Marketplace/Marketplace.jsx
@@ -73,29 +73,27 @@ const Marketplace = ({ searchInput }) => {
             return <Item key={i.item_id} item={i} />;
           })}
       </div>
-      {items.length >= 12 && (
-        <div className="items-list__pagination">
-          <button
-            disabled={currentPage === 1}
-            onClick={() => {
-              goToPage(currentPage - 1);
-              window.scrollTo(0, 0);
-            }}
-          >
-            Previous
-          </button>
-          {renderPagination()}
-          <button
-            disabled={currentPage === numPages}
-            onClick={() => {
-              goToPage(currentPage + 1);
-              window.scrollTo(0, 0);
-            }}
-          >
-            Next
-          </button>
-        </div>
-      )}
+      <div className="items-list__pagination">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => {
+            goToPage(currentPage - 1);
+            window.scrollTo(0, 0);
+          }}
+        >
+          Previous
+        </button>
+        {renderPagination()}
+        <button
+          disabled={currentPage === numPages}
+          onClick={() => {
+            goToPage(currentPage + 1);
+            window.scrollTo(0, 0);
+          }}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
